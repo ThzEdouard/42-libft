cfe4d6bfc27fd13be39b58673924b1fb2b9a3536
ThzEdouard <98763267+ThzEdouard@users.noreply.github.com>
2022-05-07 22:40:47 +0200

    push

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -16,8 +16,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	if (dest == NULL || src == NULL)
-		return (dest);
+	if (!dest && !src)
+		return (0);
 	i = 0;
 	while (i < n)
 	{
